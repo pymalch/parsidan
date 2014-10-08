f05b57610300dffd5b450ff7d34f5e06a9b4b42a
pylover <vahid.mardani@gmail.com>
2014-10-08 21:52:16 +0330

    Internationalized sass

--- parsidan/__init__.py
+++ parsidan/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """The parsidan package"""
 import warnings
+
 warnings.filterwarnings('ignore', message='Not importing directory .*', module='tw2.core.validation')
+warnings.filterwarnings('ignore', category=DeprecationWarning, module='tgext.crud.validators')
 
