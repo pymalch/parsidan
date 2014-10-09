060da84229c6fd7ddda7a9a249625653856e9b69
pylover <vahid.mardani@gmail.com>
2014-10-09 13:31:55 +0330

    nothing

--- parsidan/__init__.py
+++ parsidan/__init__.py
@@ -4,4 +4,5 @@ import warnings
 
 warnings.filterwarnings('ignore', message='Not importing directory .*', module='tw2.core.validation')
 warnings.filterwarnings('ignore', category=DeprecationWarning, module='tgext.crud.validators')
+warnings.filterwarnings('ignore', category=DeprecationWarning, module='sprox.validators')
 
