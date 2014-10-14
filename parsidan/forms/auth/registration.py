ab81c070a38021be55ab299c8a5d5d9ee91f085f
pylover <vahid.mardani@gmail.com>
2014-10-14 11:01:35 +0330

    Recaptcha validation bug fixed

--- parsidan/forms/auth/registration.py
+++ parsidan/forms/auth/registration.py
@@ -10,8 +10,9 @@ from formencode.validators import FieldsMatch, String as StringValidator
 
 class RegistrationForm(BaseForm):
     email = twf.TextField(label=l_('Email'), validator=twc.EmailValidator(required=True))
-    password = twf.PasswordField(label=l_('Password'), validator=StringValidator(min=8, max=20))
-    password_confirm = twf.PasswordField(label=l_('Password Confirm'), validator=twc.Required)
+    # password = twf.PasswordField(label=l_('Password'), validator=StringValidator(min=8, max=20))
+    # password_confirm = twf.PasswordField(label=l_('Password Confirm'), validator=twc.Required)
+    # recaptcha_challenge_field = twf.HiddenField()
     recaptcha_response_field = FixedReCaptcha(label=l_("Please Enter these words"),
                                                   public_key=tg.config.get('recaptcha.public_key'),
                                                   validator=ReCaptchaValidator(tg.config.get('recaptcha.private_key'),
@@ -19,4 +20,4 @@ class RegistrationForm(BaseForm):
 
     action = '/signup'
     submit = twf.SubmitButton(id='submit', value=l_('SignUp'))
-    validator = FieldsMatch('password', 'password_confirm')
+    #validator = FieldsMatch('password', 'password_confirm')
--- parsidan/widgets/recaptcha.py
+++ parsidan/widgets/recaptcha.py
@@ -3,10 +3,17 @@ __author__ = 'vahid'
 
 
 from tw2.recaptcha import ReCaptchaWidget
-from tw2.recaptcha.validator import ReCaptchaValidator
+from tw2.recaptcha.validator import ReCaptchaValidator as BadReCaptchaValidator
 
 class FixedReCaptcha(ReCaptchaWidget):
     """
     Hack by Vahid Mardani, due the tw2.recaptcha validation bug
     """
     _sub_compound = True
+    pass
+
+class ReCaptchaValidator(BadReCaptchaValidator):
+    """
+    Hack by Vahid Mardani, due the tw2.recaptcha validation bug
+    """
+    not_empty = True
