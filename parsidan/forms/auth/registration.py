9287d5c1c3bcfed34566a4de4a1275270e602a51
pylover <vahid.mardani@gmail.com>
2014-10-14 10:22:57 +0330

    Registration form server side validation.

--- parsidan/forms/auth/registration.py
+++ parsidan/forms/auth/registration.py
@@ -6,12 +6,12 @@ import tw2.core as twc
 import tg
 from tg.i18n import lazy_ugettext as l_
 from parsidan.widgets import BaseForm, ReCaptchaValidator, FixedReCaptcha
-from formencode.validators import FieldsMatch
+from formencode.validators import FieldsMatch, String as StringValidator
 
 class RegistrationForm(BaseForm):
-    email = twf.TextField(label=l_('Email'), validator=twc.EmailValidator)
-    password = twf.PasswordField(label=l_('Password'))
-    password_confirm = twf.PasswordField(label=l_('Password Confirm'))
+    email = twf.TextField(label=l_('Email'), validator=twc.EmailValidator(required=True))
+    password = twf.PasswordField(label=l_('Password'), validator=StringValidator(min=8, max=20))
+    password_confirm = twf.PasswordField(label=l_('Password Confirm'), validator=twc.Required)
     recaptcha_response_field = FixedReCaptcha(label=l_("Please Enter these words"),
                                                   public_key=tg.config.get('recaptcha.public_key'),
                                                   validator=ReCaptchaValidator(tg.config.get('recaptcha.private_key'),
