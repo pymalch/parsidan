bc15b6b7818a1a2e4cfcc4eb1d4afaed05b5d761
masoud <malekiciw@gmail.com>
2014-10-16 15:50:01 +0330

    #77

--- parsidan/forms/auth/registration.py
+++ parsidan/forms/auth/registration.py
@@ -9,7 +9,7 @@ from parsidan.widgets import BaseForm, ReCaptchaValidator, FixedReCaptcha
 from formencode.validators import FieldsMatch, String as StringValidator
 
 class RegistrationForm(BaseForm):
-    email = twf.TextField(label=l_('Email'), validator=twc.EmailValidator(required=True))
+    email = twf.TextField(label=l_('Email'), validator=twc.EmailValidator(required=True,msgs={'bademail':_('Must be a valid email address')}))
     nickname = twf.TextField(label=l_('Nickname'))
     password = twf.PasswordField(label=l_('Password'), validator=StringValidator(min=8, max=20))
     password_confirm = twf.PasswordField(label=l_('Password Confirm'), validator=twc.Required)
