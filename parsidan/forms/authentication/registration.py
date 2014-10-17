0e6a242e6adb501313ba08063d0ab45d9f0b5f35
masoud <malekiciw@gmail.com>
2014-10-17 15:39:43 +0330

    #85 Done

--- parsidan/forms/authentication/registration.py
+++ parsidan/forms/authentication/registration.py
@@ -6,15 +6,14 @@ import tw2.core as twc
 import tg
 from tg.i18n import lazy_ugettext as l_
 from parsidan.widgets import BaseForm, ReCaptchaValidator, FixedReCaptcha
-from formencode.validators import FieldsMatch, String as StringValidator
 
 class RegistrationForm(BaseForm):
     email = twf.TextField(label=l_('Email'),
                           validator=twc.EmailValidator(required=True,
                                                        msgs={'bademail':l_('Must be a valid email address')}))
     nickname = twf.TextField(label=l_('Nickname'))
-    password = twf.PasswordField(label=l_('Password'), validator=StringValidator(min=8, max=20))
-    password_confirm = twf.PasswordField(label=l_('Password Confirm'), validator=twc.Required)
+    password = twf.PasswordField(label=l_('Password'), validator=twc.LengthValidator(min=8, max=20))
+    password_confirm = twf.PasswordField(label=l_('Password Confirm'), validator=twc.MatchValidator('password_confirm'))
     recaptcha_challenge_field = twf.HiddenField()
     recaptcha_response_field = FixedReCaptcha(label=l_('Please Enter whatever you see in the below'),
                                                   public_key=tg.config.get('recaptcha.public_key'),
@@ -23,4 +22,3 @@ class RegistrationForm(BaseForm):
 
     action = '/authentication/signup'
     submit = twf.SubmitButton(id='submit', value=l_('Sign Up'))
-    validator = FieldsMatch('password', 'password_confirm')
