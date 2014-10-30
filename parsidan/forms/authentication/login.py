cd6a826bd68eee5977b5cef07f70d83c973e125f
masoud <malekiciw@gmail.com>
2014-10-31 02:14:17 +0330

    #97 Done

--- parsidan/forms/authentication/login.py
+++ parsidan/forms/authentication/login.py
@@ -9,8 +9,8 @@ from parsidan.widgets import BaseForm, ReCaptchaValidator, FixedReCaptcha
 
 
 class LoginForm(BaseForm):
-    login = twf.TextField(label=l_('Email'))
-    password = twf.PasswordField(label=l_('Password'))
+    login = twf.TextField(label=l_('Email'),placeholder=l_('Email'), input_group={'right':'glyphicon glyphicon-envelope'})
+    password = twf.PasswordField(label=l_('Password'),placeholder=l_('Password'), input_group={'right':'glyphicon glyphicon-lock'})
     # recaptcha_response_field = FixedReCaptcha(label=l_("Please Enter these words"),
     #                                               public_key=tg.config.get('recaptcha.public_key'),
     #                                               validator=ReCaptchaValidator(tg.config.get('recaptcha.private_key'),
