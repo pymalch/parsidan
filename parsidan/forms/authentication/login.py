567718d46518999b9b22270bac2be10f0dd6b08e
masoud <malekiciw@gmail.com>
2014-11-15 00:34:51 +0330

    add validation messages to form models

--- parsidan/forms/authentication/login.py
+++ parsidan/forms/authentication/login.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 __author__ = 'vahid'
 
-
-
 import tw2.bootstrap.forms as twf
 from tg.i18n import lazy_ugettext as l_
 from parsidan.widgets import BaseForm, ReCaptchaValidator, FixedReCaptcha
 
 
 class LoginForm(BaseForm):
-    login = twf.TextField(label=l_('Email'),placeholder=l_('Email'), addon_icon={'right':'glyphicon glyphicon-envelope'})
-    password = twf.PasswordField(label=l_('Password'), placeholder=l_('Password'), addon_icon={'right':'glyphicon glyphicon-lock'})
+    login = twf.TextField(label=l_('Email'), placeholder=l_('Email'),
+                          addon_icon={'right': 'glyphicon glyphicon-envelope'})
+    password = twf.PasswordField(label=l_('Password'), placeholder=l_('Password'),
+                                 addon_icon={'right': 'glyphicon glyphicon-lock'})
     # recaptcha_response_field = FixedReCaptcha(label=l_("Please Enter these words"),
     #                                               public_key=tg.config.get('recaptcha.public_key'),
     #                                               validator=ReCaptchaValidator(tg.config.get('recaptcha.private_key'),
--- parsidan/forms/validation.py
+++ parsidan/forms/validation.py
@@ -6,5 +6,8 @@ from tg.i18n import lazy_ugettext as l_
 messages = {
     'string_tooshort': l_('Please enter at lease %s characters.') % 8,
     'string_toolong': l_('The maximum characters allowed are %s.') % 20,
-    'required': l_('Enter a value')
+    'required': l_('Enter a value'),
+    'bademail':l_('Must be a valid email address'),
+    'mismatch' :l_('Must match Password')
+
 }
